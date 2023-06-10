f3783d57645d62cc5befd4fcd162ba02142ba80a
yukimasaki <woodenhoe@gmail.com>
2023-06-10 06:12:06 +0000

    update: var -> constへ変更

--- src/aifuku-clone/backend/ts/controllers/message.ts
+++ src/aifuku-clone/backend/ts/controllers/message.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express'
 
-var router: Router = express.Router();
+const router: Router = express.Router();
 
 router.get('/', function(req, res) {
     res.send({text:"こんにちは！ビルドしました！"});
