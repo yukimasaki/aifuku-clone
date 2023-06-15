4ed46b8fc85bd79cb58c0c05983a6181e629f711
yukimasaki <woodenhoe@gmail.com>
2023-06-15 01:31:45 +0000

    delete: 全削除

--- Makefile
+++
@@ -1,8 +0,0 @@
-up:
-	docker compose up -d
-build:
-	docker compose build
-sh:
-	docker compose exec aifuku-clone sh
-rm:
-	docker rm aifuku-test
--- src/aifuku-clone/backend/ts/controllers/message.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express'
-
-const router: Router = express.Router();
-
-router.get('/', function(req, res) {
-    res.send({text:"こんにちは！ビルドしました！"});
-});
-
-export default router;
