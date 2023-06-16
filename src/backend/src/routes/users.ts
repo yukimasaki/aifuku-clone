a7597a4ec18f0fded732ccabe1e5c6733f46df96
yukimasaki <woodenhoe@gmail.com>
2023-06-16 08:21:49 +0000

    add: UserのREST APIを作成

---
+++ src/backend/src/routes/users.ts
@@ -0,0 +1,48 @@
+import express from 'express'
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+const router = express.Router()
+
+// GET /users
+router.get('/', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.status(200).json({ users })
+})
+
+// GET /users:id
+router.get('/:id', async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(req.params?.id) },
+    })
+    res.status(200).json({ user })
+})
+
+// POST /users
+router.post("/", async (req, res) => {
+    const { name, email } = req.body
+    const user = await prisma.user.create({
+      data: { name, email },
+    })
+    res.status(200).json({ user })
+})
+
+// PUT /users/:id
+router.put("/:id", async (req, res) => {
+    const { name, email } = req.body
+    const user = await prisma.user.update({
+      where: { id: parseInt(req.params?.id) },
+      data: { name, email },
+    })
+    res.status(200).json({ user })
+})  
+
+// DELETE /users/:id
+router.delete("/:id", async (req, res) => {
+    const user = await prisma.user.delete({
+      where: { id: parseInt(req.params?.id) },
+    })
+    res.status(200).json({ user })
+})
+
+export default router
