0d5395c8ae2829acc84304c60ccc5ae7913f835e
yukimasaki <woodenhoe@gmail.com>
2023-06-16 08:21:22 +0000

    update: usersへのルーティングを追加

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 
 import indexRouter from './routes'
 import helloworldRouter from './routes/helloworld'
+import userRouter from './routes/users'
 
 const app = express()
 app.use(cors())
@@ -13,6 +14,7 @@ const port = 3000
 
 app.use('/', indexRouter)
 app.use('/helloworld', helloworldRouter)
+app.use('/users', userRouter)
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`)
