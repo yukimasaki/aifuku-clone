a0cce0c9967288d621c7208bd491424db9a69745
yukimasaki <woodenhoe@gmail.com>
2023-06-19 03:14:17 +0000

    update: Firebase Admin SDKインストール

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import FirebaseAdmin from 'firebase-admin'
 
 import indexRouter from './routes'
 import helloworldRouter from './routes/helloworld'
@@ -9,12 +10,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 const port = 3000
 
 app.use('/', indexRouter)
 app.use('/helloworld', helloworldRouter)
 app.use('/users', userRouter)
+
+FirebaseAdmin.initializeApp({
+  credential: FirebaseAdmin.credential.applicationDefault(),
+  databaseURL: process.env.GOOGLE_APPLICATION_DATABASE
+})
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`)
