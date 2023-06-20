d39b3373f3c43c5876a64c2c519d732c5c7880af
yukimasaki <woodenhoe@gmail.com>
2023-06-20 02:28:51 +0000

    update: tailwind.config.js

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     // Docs: https://tailwindcss.com/docs/typography-plugin
     require("@tailwindcss/typography"),
+    // Docs: https://github.com/tailwindlabs/tailwindcss-forms
+    require("@tailwindcss/forms"),
+    // Docs: https://github.com/tailwindlabs/tailwindcss-aspect-ratio
+    require("@tailwindcss/line-clamp"),
+    // Docs: https://github.com/tailwindlabs/tailwindcss-line-clamp
+    require("@tailwindcss/aspect-ratio"),
     // Docs: https://daisyui.com/docs
     require("daisyui"),
   ],
