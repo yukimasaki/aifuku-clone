ac43b8fd681a5c6b4eef55e473e10fde2efd7ba7
yukimasaki <woodenhoe@gmail.com>
2023-06-20 09:23:06 +0000

    update: ヘッダーの中身を実装中

--- src/frontend/components/HeaderComponent.vue
+++ src/frontend/components/HeaderComponent.vue
@@ -1,4 +1,13 @@
 <template>
-  <div>
-  </div>
+  <header class="sticky top-0 z-999 flex w-full bg-base-200 drop-shadow-1">
+    <div class="flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11">
+      <!-- Hamburger Toggle Button -->
+      <div class="flex items-center gap-2 sm:gap-4 lg:hidden">
+        <button class="btn btn-square z-99999 bg-base-100 p-1 shadow-sm lg:hidden">
+          三
+        </button>
+      </div>
+      <!-- Hamburger Toggle Button -->
+    </div>
+  </header>
 </template>
