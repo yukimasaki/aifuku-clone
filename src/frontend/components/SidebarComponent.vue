7bc62531033684762e3e703231803fe415558f11
yukimasaki <woodenhoe@gmail.com>
2023-06-20 09:08:32 +0000

    update: メニューに「ホーム」を追加

--- src/frontend/components/SidebarComponent.vue
+++ src/frontend/components/SidebarComponent.vue
@@ -34,21 +34,22 @@ export default {
     const logoPath = '../assets/images/sidebar'
 
     const items = reactive([
-      { id: 1, label: 'ユーザ管理', link: '', logo: `${logoPath}/user.svg` },
-      { id: 2, label: 'お知らせ', link: '', logo: `${logoPath}/information.svg` },
-      { id: 3, label: '会議・研修案内', link: '', logo: `${logoPath}/meeting.svg` },
-      { id: 4, label: '掲示板', link: '', logo: `${logoPath}/bbs.svg` },
-      { id: 5, label: '各種ニュース(R)', link: '', logo: `${logoPath}/news.svg` },
-      { id: 6, label: '各種ニュース', link: '', logo: `${logoPath}/news.svg` },
-      { id: 7, label: 'アンケート', link: '', logo: `${logoPath}/enquete.svg` },
-      { id: 8, label: '議事録・報告', link: '', logo: `${logoPath}/report.svg` },
-      { id: 9, label: 'メッセージ', link: '', logo: `${logoPath}/message.svg` },
-      { id: 10, label: '各種資料', link: '', logo: `${logoPath}/document.svg` },
-      { id: 11, label: '書籍紹介', link: '', logo: `${logoPath}/bookinformation.svg` },
-      { id: 12, label: '月間スケジュール', link: '', logo: `${logoPath}/schedule.svg` },
-      { id: 13, label: '支援物資要請', link: '', logo: `${logoPath}/rescue.svg` },
-      { id: 14, label: '被害状況確認', link: '', logo: `${logoPath}/disaster.svg` },
-      { id: 15, label: '各種設定', link: '', logo: `${logoPath}/setting.svg` },
+      { id: 1, label: 'ホーム', link: '', logo: `${logoPath}/home.svg` },
+      { id: 2, label: 'ユーザ管理', link: '', logo: `${logoPath}/user.svg` },
+      { id: 3, label: 'お知らせ', link: '', logo: `${logoPath}/information.svg` },
+      { id: 4, label: '会議・研修案内', link: '', logo: `${logoPath}/meeting.svg` },
+      { id: 5, label: '掲示板', link: '', logo: `${logoPath}/bbs.svg` },
+      { id: 6, label: '各種ニュース(R)', link: '', logo: `${logoPath}/news.svg` },
+      { id: 7, label: '各種ニュース', link: '', logo: `${logoPath}/news.svg` },
+      { id: 8, label: 'アンケート', link: '', logo: `${logoPath}/enquete.svg` },
+      { id: 9, label: '議事録・報告', link: '', logo: `${logoPath}/report.svg` },
+      { id: 10, label: 'メッセージ', link: '', logo: `${logoPath}/message.svg` },
+      { id: 11, label: '各種資料', link: '', logo: `${logoPath}/document.svg` },
+      { id: 12, label: '書籍紹介', link: '', logo: `${logoPath}/bookinformation.svg` },
+      { id: 13, label: '月間スケジュール', link: '', logo: `${logoPath}/schedule.svg` },
+      { id: 14, label: '支援物資要請', link: '', logo: `${logoPath}/rescue.svg` },
+      { id: 15, label: '被害状況確認', link: '', logo: `${logoPath}/disaster.svg` },
+      { id: 16, label: '各種設定', link: '', logo: `${logoPath}/setting.svg` },
     ])
 
     return {
