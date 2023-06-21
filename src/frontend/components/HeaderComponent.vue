bc804cd9da03428d5ce7c7a9161433e6f9cf4d26
yukimasaki <woodenhoe@gmail.com>
2023-06-21 07:58:29 +0000

    fix: ヘッダーのレイアウト調整

--- src/frontend/components/HeaderComponent.vue
+++ src/frontend/components/HeaderComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="sticky top-0 z-999 flex w-full bg-base-200 drop-shadow-1">
+  <header class="sticky top-0 z-999 flex w-full bg-base-100 drop-shadow-1">
     <div class="flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11">
       <!-- Hamburger Toggle Button -->
       <div class="flex items-center gap-2 sm:gap-4 lg:hidden">
@@ -11,6 +11,20 @@
         </button>
       </div>
       <!-- Hamburger Toggle Button -->
+
+      <!-- Middle Area -->
+      <div class="hidden sm:block">
+      </div>
+      <!-- Middle Area -->
+
+      <!-- Right Area -->
+      <div class="flex items-center gap-3 2xsm:gap-7">
+        <ul class="flex items-center gap-2 2xsm:gap-4">
+          <li class="relative"><a>〇</a></li>
+          <li class="relative"><a>★</a></li>
+        </ul>
+      </div>
+      <!-- Right Area -->
     </div>
   </header>
 </template>
