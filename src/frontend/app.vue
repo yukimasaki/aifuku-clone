b60021aba87efbf5b0d427d1ea605def1608d5da
yukimasaki <woodenhoe@gmail.com>
2023-06-22 03:23:53 +0000

    update: NuxtLayoutに書き換え

--- src/frontend/app.vue
+++ src/frontend/app.vue
@@ -1,13 +1,3 @@
 <template>
-  <div class="flex h-screen overflow-hidden">
-    <SidebarComponent />
-    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-      <HeaderComponent />
-      <NuxtPage />
-    </div>
-  </div>
+  <NuxtLayout />
 </template>
-
-<script>
-
-</script>
