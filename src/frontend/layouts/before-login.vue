44d2ccb4eb18547acbb0c277a9756fee287c8c37
yukimasaki <woodenhoe@gmail.com>
2023-06-22 03:38:36 +0000

    add: ログイン前のヘッダーを追加

--- src/frontend/layouts/before-login.vue
+++ src/frontend/layouts/before-login.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="flex h-screen overflow-hidden">
-    <NuxtPage />
+    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <HeaderBeforeLogin />
+      <NuxtPage />
+    </div>
   </div>
   <slot />
 </template>
+
+<script setip>
+import HeaderBeforeLogin from '~/components/HeaderBeforeLogin.vue'
+</script>
