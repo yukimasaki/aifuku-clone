ec757e6bd2a2c1c5977e12d94d0ce56b03631a51
yukimasaki <woodenhoe@gmail.com>
2023-06-22 12:36:25 +0000

    update: firebaseプラグインのインストールに難航中

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -5,6 +5,13 @@ export default defineNuxtConfig({
     // Docs: https://tailwindcss.nuxtjs.org
     "@nuxtjs/tailwindcss",
   ],
+  runtimeConfig: {
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || "",
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || "",
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || "",
+    }
+  },
   app: {
     head: {
       htmlAttrs: {
---
+++ src/frontend/plugins/firebase.client.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app'
+
+export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig()
+  const firebaseConfig = {
+    apiKey: config.public.FIREBASE_API_KEY,
+    authDomain: config.public.FIREBASE_AUTH_DOMAIN,
+    projectId: config.public.FIREBASE_PROJECT_ID,
+  }
+  initializeApp(firebaseConfig)
+})
