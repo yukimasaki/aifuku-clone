31a3826862bc3896e2e94ff78761cdea8d1cb8b9
yukimasaki <woodenhoe@gmail.com>
2023-06-22 06:52:45 +0000

    update: カラーコードを変数に格納

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,8 +1,26 @@
+const { access } = require('fs')
+
 /** @type {import('tailwindcss').Config} */
+const customColor = {
+  primary: '#5f939a',
+  secondary: "#1b2021",
+  accent: "#d8ac9c",
+  neutral: "#eac8af",
+  base: "#eff4f4",
+  info: "#4e8dda",
+  success: "#1a7465",
+  warning: "#9f730f",
+  error: "#f33f42",
+}
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "daisy-primary": customColor.primary,
+      }
+    },
   },
   plugins: [
     // Docs: https://tailwindcss.com/docs/typography-plugin
@@ -20,15 +38,15 @@ module.exports = {
     themes: [
       {
         aifuku: {
-          "primary": "#5f939a",
-          "secondary": "#1b2021",
-          "accent": "#d8ac9c",
-          "neutral": "#eac8af",
-          "base-100": "#eff4f4",
-          "info": "#4e8dda",
-          "success": "#1a7465",
-          "warning": "#9f730f",
-          "error": "#f33f42",
+          "primary": customColor.primary,
+          "secondary": customColor.secondary,
+          "accent": customColor.accent,
+          "neutral": customColor.neutral,
+          "base-100": customColor.base,
+          "info": customColor.info,
+          "success": customColor.success,
+          "warning": customColor.warning,
+          "error": customColor.error,
         }
       }
     ]
