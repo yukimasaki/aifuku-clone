c7e191a6e784b0fcc12cf66c555ce7b6d13c1efd
yukimasaki <woodenhoe@gmail.com>
2023-06-22 03:38:02 +0000

    update: import文を挿入

--- src/frontend/layouts/after-login.vue
+++ src/frontend/layouts/after-login.vue
@@ -8,3 +8,9 @@
     <slot />
   </div>
 </template>
+
+<script setup>
+import SidebarAfterLogin from '~/components/SidebarAfterLogin.vue'
+import HeaderAfterLogin from '~/components/HeaderAfterLogin.vue'
+
+</script>
