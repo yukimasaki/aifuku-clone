e6f8e80cb1e5e418c97be77fdeb693da2fb054ba
yukimasaki <woodenhoe@gmail.com>
2023-06-22 07:46:08 +0000

    ok: API繋ぎ込み完了

--- src/frontend/pages/login.vue
+++ src/frontend/pages/login.vue
@@ -5,19 +5,19 @@
         <div class="card-body">
           <h2 class="card-title pb-4">ログイン</h2>
           <input
-            v-model="user.email"
+            v-model="email"
             type="text"
             placeholder="メールアドレス"
             class="input input-bordered w-full max-w-xs focus:outline-none focus:ring-4 focus:border-transparent focus:ring-daisy-primary"
           />
           <input
-            v-model="user.password"
+            v-model="password"
             type="password"
             placeholder="パスワード"
             class="input input-bordered w-full max-w-xs focus:outline-none focus:ring-4 focus:border-transparent focus:ring-daisy-primary"
           />
           <div class="card-actions justify-end pt-4">
-            <button class="btn btn-primary text-base-100 drop-shadow" @click="login(user)">ログイン</button>
+            <button class="btn btn-primary text-base-100 drop-shadow" @click="login(email, password)">ログイン</button>
           </div>
         </div>
       </div>
@@ -30,12 +30,21 @@ definePageMeta({
   layout: 'before-login'
 })
 
-const user = reactive({
-  email: '',
-  password: ''
-})
+const email = ''
+const password = ''
 
-const login = (user) => {
-  console.log(user)
+const url = 'http://192.168.93.150:3000/login'
+
+const login = async (email, password) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+  const resData = await res.json()
+
+  console.log(res.status, resData)
 }
 </script>
