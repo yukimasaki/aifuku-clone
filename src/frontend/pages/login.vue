025eb379a07c351db336c9af2a854fc06e1d73c5
yukimasaki <woodenhoe@gmail.com>
2023-06-22 07:03:36 +0000

    update: ログイン処理実装中

--- src/frontend/pages/login.vue
+++ src/frontend/pages/login.vue
@@ -4,10 +4,20 @@
       <div class="card bg-base-100 w-96 drop-shadow">
         <div class="card-body">
           <h2 class="card-title pb-4">ログイン</h2>
-          <input type="text" placeholder="ユーザー名" class="input input-bordered w-full max-w-xs focus:outline-none focus:ring-4 focus:border-transparent focus:ring-daisy-primary" />
-          <input type="password" placeholder="パスワード" class="input input-bordered w-full max-w-xs focus:outline-none focus:ring-4 focus:border-transparent focus:ring-daisy-primary" />
+          <input
+            v-model="user.email"
+            type="text"
+            placeholder="メールアドレス"
+            class="input input-bordered w-full max-w-xs focus:outline-none focus:ring-4 focus:border-transparent focus:ring-daisy-primary"
+          />
+          <input
+            v-model="user.password"
+            type="password"
+            placeholder="パスワード"
+            class="input input-bordered w-full max-w-xs focus:outline-none focus:ring-4 focus:border-transparent focus:ring-daisy-primary"
+          />
           <div class="card-actions justify-end pt-4">
-            <button class="btn btn-primary text-base-100 drop-shadow">ログイン</button>
+            <button class="btn btn-primary text-base-100 drop-shadow" @click="login(user)">ログイン</button>
           </div>
         </div>
       </div>
@@ -19,4 +29,13 @@
 definePageMeta({
   layout: 'before-login'
 })
+
+const user = reactive({
+  email: '',
+  password: ''
+})
+
+const login = (user) => {
+  console.log(user)
+}
 </script>
