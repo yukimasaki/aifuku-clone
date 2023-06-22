f971347936ba1bebf352d1594d43777ed25fb3b1
yukimasaki <woodenhoe@gmail.com>
2023-06-22 13:19:57 +0000

    update: appidを追加

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
       FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || "",
       FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || "",
       FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || "",
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID || "",
     }
   },
   app: {
