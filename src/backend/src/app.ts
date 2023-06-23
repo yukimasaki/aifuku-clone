c6e198374a8ba157649063fc6d53f99c262cdeba
yukimasaki <woodenhoe@gmail.com>
2023-06-23 08:08:51 +0000

    update: verifyRouterをルーティングに追加

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 
 import userRouter from './routes/users'
 import loginRouter from './routes/login'
+import verifyRouter from './routes/verify'
 
 const app = express()
 const port = 3000
@@ -15,6 +16,7 @@ app.use(cookieParser())
 
 app.use('/users', userRouter)
 app.use('/login', loginRouter)
+app.use('/verify', verifyRouter)
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`)
