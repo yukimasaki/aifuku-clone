3f47830682f1c0ac7cfbb54876e7610ab6c46cc3
yukimasaki <woodenhoe@gmail.com>
2023-06-23 07:57:45 +0000

    delete: helloworld, indexを削除

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 
-import indexRouter from './routes'
-import helloworldRouter from './routes/helloworld'
 import userRouter from './routes/users'
 import loginRouter from './routes/login'
 
@@ -13,8 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/', indexRouter)
-app.use('/helloworld', helloworldRouter)
 app.use('/users', userRouter)
 app.use('/login', loginRouter)
 
