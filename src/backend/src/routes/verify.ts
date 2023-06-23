4b9ddee998a528208acaebe5e995079295d3c8ea
yukimasaki <woodenhoe@gmail.com>
2023-06-23 08:08:12 +0000

    add: トークンの認証処理を実装

---
+++ src/backend/src/routes/verify.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+const router = express.Router()
+
+// POST /verify
+router.post('/', async (req, res) => {
+  const firebaseApiKey = 'AIzaSyDIraHkuFWYdItWEydce1dbaAwBsRNNMeA'
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${firebaseApiKey}`
+  const idToken = req.cookies.token
+  const body = JSON.stringify({ idToken })
+
+  await fetch(
+    url,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body,
+    }
+  )
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    res.status(200).json({ message: 'ok', data })
+  })
+  .catch(error => {
+    res.status(401).json({ message: 'ng', error })
+  })
+})
+
+export default router
