1cb99c18b555e30710af722d6f1b74b0d86274f8
yukimasaki <woodenhoe@gmail.com>
2023-06-23 07:56:43 +0000

    update: リファクタリング

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -7,10 +7,11 @@ import userRouter from './routes/users'
 import loginRouter from './routes/login'
 
 const app = express()
+const port = 3000
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-const port = 3000
 
 app.use('/', indexRouter)
 app.use('/helloworld', helloworldRouter)
