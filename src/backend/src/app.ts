4579f6fb613de6bd07b5101e94bf2a3e65f49877
yukimasaki <woodenhoe@gmail.com>
2023-06-23 08:06:03 +0000

    update: cookie-parserをインポート

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 import userRouter from './routes/users'
 import loginRouter from './routes/login'
@@ -7,9 +8,10 @@ import loginRouter from './routes/login'
 const app = express()
 const port = 3000
 
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(cookieParser())
 
 app.use('/users', userRouter)
 app.use('/login', loginRouter)
