d90bff5afdfbefa979b7613c8799629446130285
yukimasaki <woodenhoe@gmail.com>
2023-06-23 06:07:00 +0000

    update: middleware削除に伴う修正

--- src/frontend/pages/index.vue
+++ src/frontend/pages/index.vue
@@ -5,7 +5,4 @@
 </template>
 
 <script setup>
-definePageMeta({
-  middleware: 'get-current-user',
-})
 </script>
--- src/frontend/plugins/firebase.client.ts
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from 'firebase/app'
-
-export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
-  initializeApp(config.public.firebase)
-})
