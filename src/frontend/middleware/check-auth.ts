6e4240850100d0b0840c6bfa342579e2d1e3bbd3
yukimasaki <woodenhoe@gmail.com>
2023-06-26 10:11:33 +0000

    issue: 認証用ミドルウェア実装、500エラー発生

--- src/frontend/middleware/check-auth.ts
+++ src/frontend/middleware/check-auth.ts
@@ -1,9 +1,19 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   const url = '/api/verify'
-  await fetch(
+  const response = await fetch(
     url,
     {
       method: 'GET',
     }
   )
+
+  console.log(response)
+
+  if (!response.ok) {
+    console.log(`ログインしていません。`)
+    // return navigateTo('/login', { replace: true })
+  } else {
+    console.log(`ログインしています。`)
+    // return navigateTo('/test')
+  }
 })
