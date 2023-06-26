dad03866f2ae90c0569cbe325f6f649aaca1bfee
yukimasaki <woodenhoe@gmail.com>
2023-06-26 05:36:13 +0000

    ok: users.get.ts実装

---
+++ src/frontend/server/api/users.get.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client'
+
+export default defineEventHandler(async () => {
+  const prisma = new PrismaClient()
+  const users = await prisma.user.findMany()
+
+  return JSON.stringify({ users })
+})
