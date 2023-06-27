eed5f9f69ff18bb92766363347b66cd49f21d990
yukimasaki <woodenhoe@gmail.com>
2023-06-27 07:55:40 +0000

    update: logout.postをトークン認証に対応させた

--- src/frontend/server/api/logout.post.ts
+++ src/frontend/server/api/logout.post.ts
@@ -1,10 +1,19 @@
+import { useAuth } from '../../composables/useAuth'
+
 export default defineEventHandler(async (event) => {
-  deleteCookie(
-    event,
-    'token',
-    {
-    httpOnly: true,
-    }
-  )
-  return true
+  const { fetchUserWithToken } = useAuth()
+  const response = await fetchUserWithToken(event)
+
+  if (!response.ok) {
+    return false
+  } else {
+    deleteCookie(
+      event,
+      'token',
+      {
+      httpOnly: true,
+      }
+    )
+    return true
+  }
 })
