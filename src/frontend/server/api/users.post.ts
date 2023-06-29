a588f06beacb52f493b4e5b382366fa0adf02112
yukimasaki <woodenhoe@gmail.com>
2023-06-29 05:57:59 +0000

    todo: ユーザ登録処理のAPI実装完了、要テスト

--- src/frontend/server/api/users.post.ts
+++ src/frontend/server/api/users.post.ts
@@ -1,8 +1,11 @@
 import { getAuth, createUserWithEmailAndPassword, deleteUser } from 'firebase/auth'
 import { PrismaClient } from '@prisma/client'
+import { useErrorHande } from '../../composables/useErrorHandle'
 
 export default defineEventHandler(async (event) => {
   const prisma = new PrismaClient()
+  const { firebaseErrorMessageToHttpStatusCode } = useErrorHande()
+
   const req = await readBody(event)
   const { email, password, tenantId, displayName } = req
 
@@ -18,6 +21,7 @@ export default defineEventHandler(async (event) => {
     const auth = getAuth()
     const userCredential = await createUserWithEmailAndPassword(auth, email, password)
     const uid = userCredential.user.uid
+    console.log(`Firebaseへの登録に成功`)
     // Firebaseへのユーザ登録後、データベースにもプロフィール情報を登録する
     try {
       const profile = await prisma.profile.create({
@@ -28,53 +32,38 @@ export default defineEventHandler(async (event) => {
           tenantId: parseInt(tenantId),
         }
       })
-      console.log(profile)
+      // final. 双方のユーザ登録に成功した場合はユーザ情報を含むJSONデータを返す
+      console.log(`データベースへの登録に成功`)
+      return JSON.stringify({
+        uid: profile.uid,
+        email: profile.email,
+        displayName: profile.displayName,
+      })
     } catch (error) {
-      // データベースへの登録に失敗した場合は、Firebaseへ登録したユーザ情報を削除する
-      console.log(`データベースへの登録に失敗`)
-      console.log(error)
       try {
+        // データベースへの登録に失敗した場合は、Firebaseへ登録したユーザ情報を削除する
         await deleteUser(userCredential.user)
+        console.log(`データベースへの登録に失敗`)
+        console.log(error)
+        throw createError({
+          statusCode: 400,
+          statusMessage: 'Bad Request',
+        })
       } catch (error) {
         console.log(`Firebaseからの削除に失敗`)
         console.log(error)
+        throw createError({
+          statusCode: 500,
+          statusMessage: 'Internal Server Error',
+        })
       }
     }
-    // final. 双方のユーザ登録に成功した場合はユーザ情報を含むJSONデータを返す
-    const user = userCredential.user
-    return JSON.stringify({
-      uid: user.uid,
-      email: user.email,
-    })
   } catch (error: any) {
     // ユーザ登録に失敗した場合は、Firebaseのエラーコードに応じてステータスコードとステータスメッセージを返す
+    console.log(`Firebaseへの登録に失敗`)
+    console.log(error)
     const message = error.code
-    let statusCode
-    let statusMessage
-
-    switch (message) {
-      case 'auth/invalid-email':
-      case 'auth/weak-password':
-        statusCode = 400
-        statusMessage = 'Bad Request'
-        break
-
-      case 'auth/operation-not-allowed':
-        statusCode = 403
-        statusMessage = 'Forbidden'
-        break
-
-      case 'auth/email-already-in-use':
-        statusCode = 409
-        statusMessage = 'Conflict'
-        break
-
-      default:
-        statusCode = 500
-        statusMessage = 'Internal Server Error'
-        break
-    }
-
+    const { statusCode, statusMessage } = firebaseErrorMessageToHttpStatusCode(message)
     throw createError({
       statusCode,
       statusMessage,
