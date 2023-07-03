90fdd8d747dd5fd7fc1698871409e8f74046c77f
yukimasaki <woodenhoe@gmail.com>
2023-07-03 00:56:43 +0000

    feat: user.post.tsに準拠したコードへ修正

--- src/frontend/server/api/login.post.ts
+++ src/frontend/server/api/login.post.ts
@@ -1,9 +1,8 @@
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { useErrorHandle } from '../../composables/useErrorHandle'
+import validator from 'validator'
 
 export default defineEventHandler(async (event) => {
-  const { firebaseErrorMessageToHttpStatusCode } = useErrorHandle()
-
   const req = await readBody(event)
   const { email, password } = req
 
@@ -12,25 +11,61 @@ export default defineEventHandler(async (event) => {
     throw createError({
       statusCode: 400,
       statusMessage: 'Bad Request',
+      message: 'Invalid request body',
+    })
+  }
+
+  // バリデーションを行い、1つでも不合格の場合は例外をスローし、全てに合格した場合は処理を続行する
+  const validationResult = valid(email, password)
+  if (!validationResult) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request',
+      message: 'Validation failed',
     })
   }
 
   try {
-    const auth = getAuth()
-    const userCredential = await signInWithEmailAndPassword(auth, email, password)
-    const user = userCredential.user
-    // ログインに成功した場合はユーザ情報を含むJSONデータを返す
-    return JSON.stringify({
-      uid: user.uid,
-      email: user.email,
-    })
+    // ログインに成功したらuidを含むJSONデータを返し、失敗したら例外をスローする
+    const uid = await login(email, password)
+    return uid
   } catch (error: any) {
-    // ログインに失敗した場合は、Firebaseのエラーコードに応じてステータスコードとステータスメッセージを返す
-    const message = error.code
-    const { statusCode, statusMessage } = firebaseErrorMessageToHttpStatusCode(message)
-    throw createError({
-      statusCode,
-      statusMessage,
-    })
+    onFailureLogin(error)
   }
 })
+
+const valid = (
+  email: any,
+  password: any,
+) => {
+  const ruleEmail = () => validator.isEmail(email)
+  const rulePassword = () => validator.isStrongPassword(password, { minLength: 6 })
+
+  const validationResult = [
+    ruleEmail(),
+    rulePassword(),
+  ].every(result => result === true)
+
+  return validationResult
+}
+
+const login = async (email: string, password: string) => {
+  console.log(`login`)
+  const auth = getAuth()
+  const userCredential = await signInWithEmailAndPassword(auth, email, password)
+  const uid = userCredential.user.uid
+
+  return JSON.stringify({ uid })
+}
+
+const onFailureLogin = (error: any) => {
+  console.log(`onFailureLogin`)
+  const { firebaseErrorMessageToHttpStatusCode } = useErrorHandle()
+  const message = error.code
+  const { statusCode, statusMessage } = firebaseErrorMessageToHttpStatusCode(message)
+  throw createError({
+    statusCode,
+    statusMessage,
+    message: 'Login failed',
+  })
+}
