63734ffea89098dc92eb8ba23254ec22d6f138ff
yukimasaki <woodenhoe@gmail.com>
2023-07-03 06:28:40 +0000

    feat: Firebase Auth REST APIへの接続処理をcomposableで作成

---
+++ src/frontend/composables/useFirebase.ts
@@ -0,0 +1,45 @@
+export const useFirebase = () => {
+  const apiKey = 'AIzaSyDIraHkuFWYdItWEydce1dbaAwBsRNNMeA'
+  const baseUrl = `https://identitytoolkit.googleapis.com/v1`
+
+  const signUp = async (email: string, password: string) => {
+    const endPoint = `accounts:signUp`
+    const url = `${baseUrl}/${endPoint}?key=${apiKey}`
+
+    const body = {
+      email,
+      password,
+      returnSecureToken: true,
+    }
+
+    const response = await fetch(url,{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+
+    return await response.json()
+  }
+
+  const deleteUser = async (idToken: string) => {
+    const endPoint = `accounts:delete`
+    const url = `${baseUrl}/${endPoint}?key=${apiKey}`
+
+    const body = {
+      idToken
+    }
+
+    const response = await fetch(url,{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+
+    return await response.json()
+  }
+
+  return {
+    signUp,
+    deleteUser,
+  }
+}
