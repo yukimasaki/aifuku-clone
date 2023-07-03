525b904cacaa6840a123b8d07ac0d570000b3124
yukimasaki <woodenhoe@gmail.com>
2023-07-03 06:48:09 +0000

    feat: Firebase Auth REST APIに対応

--- src/frontend/server/api/login.post.ts
+++ src/frontend/server/api/login.post.ts
@@ -1,84 +1,83 @@
-// import { useErrorHandle } from '../../composables/useErrorHandle'
-// import validator from 'validator'
-// import { H3Event } from 'h3'
+import validator from 'validator'
+import { H3Event } from 'h3'
+import { useFirebase } from '../../composables/useFirebase'
+import { useErrorHandle } from '../../composables/useErrorHandle'
 
-// export default defineEventHandler(async (event) => {
-//   const req = await readBody(event)
-//   const { email, password } = req
+export default defineEventHandler(async (event) => {
+  const req = await readBody(event)
+  const { email, password } = req
 
-//   // リクエストボディで渡されたJSONデータが不正な場合は400を返す
-//   if (!email || !password) {
-//     throw createError({
-//       statusCode: 400,
-//       statusMessage: 'Bad Request',
-//       message: 'Invalid request body',
-//     })
-//   }
+  // リクエストボディで渡されたJSONデータが不正な場合は400を返す
+  if (!email || !password) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request',
+      message: 'Invalid request body',
+    })
+  }
 
-//   // バリデーションを行い、1つでも不合格の場合は例外をスローし、全てに合格した場合は処理を続行する
-//   const validationResult = valid(email, password)
-//   if (!validationResult) {
-//     throw createError({
-//       statusCode: 400,
-//       statusMessage: 'Bad Request',
-//       message: 'Validation failed',
-//     })
-//   }
+  // バリデーションを行い、1つでも不合格の場合は例外をスローし、全てに合格した場合は処理を続行する
+  const validationResult = valid(email, password)
+  if (!validationResult) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request',
+      message: 'Validation failed',
+    })
+  }
 
-//   try {
-//     // ログインに成功したらuidを含むJSONデータを返し、失敗したら例外をスローする
-//     const { uid, idToken } = await login(email, password)
-//     setIdTokenToCookie(event, idToken)
+  // ログインを試みる
+  const user = await login(email, password)
+  if (user.error) {
+    onFailureLogin(user.error)
+  }
 
-//     return JSON.stringify({ uid })
-//   } catch (error: any) {
-//     onFailureLogin(error)
-//   }
-// })
+  // ログインに成功したらクッキーを保存する
+  setIdTokenToCookie(event, user.idToken)
 
-// const valid = (
-//   email: any,
-//   password: any,
-// ) => {
-//   const ruleEmail = () => validator.isEmail(email)
-//   const rulePassword = () => validator.isStrongPassword(password, { minLength: 6 })
+  return JSON.stringify({ email: user.email, uid: user.localId})
+})
 
-//   const validationResult = [
-//     ruleEmail(),
-//     rulePassword(),
-//   ].every(result => result === true)
+const valid = (
+  email: any,
+  password: any,
+) => {
+  const ruleEmail = () => validator.isEmail(email)
+  const rulePassword = () => validator.isStrongPassword(password, { minLength: 6 })
 
-//   return validationResult
-// }
+  const validationResult = [
+    ruleEmail(),
+    rulePassword(),
+  ].every(result => result === true)
 
-// const login = async (email: string, password: string) => {
-//   console.log(`login`)
-//   const auth = getAuth()
-//   const userCredential = await signInWithEmailAndPassword(auth, email, password)
+  return validationResult
+}
 
-//   const uid = userCredential.user.uid
-//   const idToken = await userCredential.user.getIdToken()
+const login = async (email: string, password: string) => {
+  console.log(`login`)
+  const { signInWithEmailAndPassword } = useFirebase()
+  const user = await signInWithEmailAndPassword(email, password)
 
-//   return { uid, idToken }
-// }
+  return user
+}
 
-// const setIdTokenToCookie = (event: H3Event, idToken: string) => {
-//   console.log(`setIdTokenToCookie`)
-//   const options = {
-//     httpOnly: true,
-//     secure: true,
-//   }
-//   setCookie(event, 'token', idToken, options)
-// }
+const setIdTokenToCookie = (event: H3Event, idToken: string) => {
+  console.log(`setIdTokenToCookie`)
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+  setCookie(event, 'token', idToken, options)
+}
 
-// const onFailureLogin = (error: any) => {
-//   console.log(`onFailureLogin`)
-//   const { firebaseErrorMessageToHttpStatusCode } = useErrorHandle()
-//   const message = error.code
-//   const { statusCode, statusMessage } = firebaseErrorMessageToHttpStatusCode(message)
-//   throw createError({
-//     statusCode,
-//     statusMessage,
-//     message: 'Login failed',
-//   })
-// }
+const onFailureLogin = (error: any) => {
+  console.log(`onFailureLogin`)
+  const { firebaseErrorMessageToHttpStatusCode } = useErrorHandle()
+  const message = error.message
+  const { statusCode, statusMessage } = firebaseErrorMessageToHttpStatusCode(message)
+  throw createError({
+    statusCode,
+    statusMessage,
+    message: 'Login failed',
+  })
+}
