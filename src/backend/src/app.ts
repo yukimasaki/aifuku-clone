6b7d86d1c26c8bd88daf6fb3c1c068797de1be2b
yukimasaki <woodenhoe@gmail.com>
2023-07-04 05:51:14 +0000

    chore: src/app.ts

---
+++ src/backend/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+import indexRouter from './routes'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const port = 3000
+
+app.use('/', indexRouter)
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`)
+})
