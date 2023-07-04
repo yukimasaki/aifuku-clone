d757abcbe87f69acd3bc7a0fc928b8afd1d4eec8
yukimasaki <woodenhoe@gmail.com>
2023-07-04 06:54:07 +0000

    feat: Firebase Auth REST APIへの接続処理をutilsに作成

---
+++ src/backend/src/utils/firebase.ts
@@ -0,0 +1,83 @@
+export const useFirebase = () => {
+  const apiKey = 'AIzaSyDIraHkuFWYdItWEydce1dbaAwBsRNNMeA'
+  const baseUrl = `https://identitytoolkit.googleapis.com/v1`
+
+  const signUp = async (email: string, password: string) => {
+    const endPoint = `accounts:signUp`
+    const url = `${baseUrl}/${endPoint}?key=${apiKey}`
+
+    const body = {
+      email,
+      password,
+      returnSecureToken: true,
+    }
+
+    const response = await fetch(url,{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+
+    return await response.json()
+  }
+
+  const deleteUser = async (idToken: string) => {
+    const endPoint = `accounts:delete`
+    const url = `${baseUrl}/${endPoint}?key=${apiKey}`
+
+    const body = {
+      idToken
+    }
+
+    const response = await fetch(url,{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+
+    return await response.json()
+  }
+
+  const signInWithEmailAndPassword = async (email: string, password: string) => {
+    const endPoint = `accounts:signInWithPassword`
+    const url = `${baseUrl}/${endPoint}?key=${apiKey}`
+
+    const body = {
+      email,
+      password,
+      returnSecureToken: true,
+    }
+
+    const response = await fetch(url,{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+
+    return await response.json()
+  }
+
+  const checkAuthState = async (idToken: string) => {
+    const endPoint = `accounts:lookup`
+    const url = `${baseUrl}/${endPoint}?key=${apiKey}`
+
+    const body = {
+      idToken,
+    }
+
+    const response = await fetch(url,{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+
+    return await response.json()
+  }
+
+  return {
+    signUp,
+    deleteUser,
+    signInWithEmailAndPassword,
+    checkAuthState,
+  }
+}
