c9cfb1b54590e5f2a9936e8f233edd52dc1b3960
yukimasaki <woodenhoe@gmail.com>
2023-07-04 07:37:24 +0000

    feat: POST /api/usersを実装

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 
 import userRouter from './routes/users'
+import loginRouter from './routes/login'
 
 const app = express()
 app.use(cors())
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 const port = 3000
 
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`)
