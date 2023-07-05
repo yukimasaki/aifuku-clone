6d96cab2be93f0736a99fbbc61967431c3e391cf
yukimasaki <woodenhoe@gmail.com>
2023-07-05 02:25:53 +0000

    GET /api/users実装

--- src/backend/src/routes/users.ts
+++ src/backend/src/routes/users.ts
@@ -2,10 +2,85 @@ import express from 'express'
 import validator from 'validator'
 import { useFirebase } from 'src/utils/firebase'
 import { PrismaClient } from '@prisma/client'
+import { verify } from '../middleware/verify'
+
 const router = express.Router()
+const prisma = new PrismaClient()
 
 /** POST /api/users */
-router.post('/', async (req, res) => {
+router.post('/', verify, async (req, res) => {
+  const valid = (
+    email: any,
+    password: any,
+    displayName: any,
+    tenantId: any,
+  ) => {
+    const ruleEmail = () => validator.isEmail(email)
+    const rulePassword = () => validator.isStrongPassword(password, { minLength: 6 })
+    const ruleDisplayName = () => {
+      const isSomeText = [
+        validator.isAscii(displayName),
+        validator.isMultibyte(displayName),
+      ].some(result => result === true)
+
+      const isValid = [
+        isSomeText,
+        validator.isLength(displayName, { min: 1, max: 32 }),
+      ].every(result => result === true)
+
+      return isValid
+    }
+    const ruleTenantId = () => validator.isInt(tenantId)
+
+    const validationResult = [
+      ruleEmail(),
+      rulePassword(),
+      ruleDisplayName(),
+      ruleTenantId(),
+    ].every(result => result === true)
+
+    return validationResult
+  }
+
+  const createUserToFirebase = async (email: string, password: string) => {
+    console.log(`createUserToFirebase`)
+    const { signUp } = useFirebase()
+    const user = await signUp(email, password)
+    return user
+  }
+
+  const onFailureCreateUserToFirebase = (error: any) => {
+    console.log(`onFailureCreateUserToFirebase`)
+    const { errMsgToStatusCodeAndMessage } = useFirebase()
+    const message = error.message
+    const { statusCode, statusMessage } = errMsgToStatusCodeAndMessage(message)
+    return { statusCode, statusMessage, message }
+  }
+
+  const createUserToDatabase = async (
+    uid: string,
+    email: string,
+    displayName: string,
+    tenantId: string,
+    ) => {
+    console.log(`createUserToDatabase`)
+    const profile = await prisma.profile.create({
+      data: {
+        uid,
+        email,
+        displayName,
+        tenantId: parseInt(tenantId),
+      }
+    })
+    return JSON.stringify(profile)
+  }
+
+  const onFailureCreateUserToDatabase = async (idToken: string) => {
+    console.log(`onFailureCreateUserToDatabase`)
+    const { deleteUser } = useFirebase()
+    await deleteUser(idToken)
+  }
+
   const body = req.body
   const { email, password, displayName, tenantId } = body
 
@@ -61,77 +136,11 @@ router.post('/', async (req, res) => {
   }
 })
 
-const valid = (
-  email: any,
-  password: any,
-  displayName: any,
-  tenantId: any,
-) => {
-  const ruleEmail = () => validator.isEmail(email)
-  const rulePassword = () => validator.isStrongPassword(password, { minLength: 6 })
-  const ruleDisplayName = () => {
-    const isSomeText = [
-      validator.isAscii(displayName),
-      validator.isMultibyte(displayName),
-    ].some(result => result === true)
-
-    const isValid = [
-      isSomeText,
-      validator.isLength(displayName, { min: 1, max: 32 }),
-    ].every(result => result === true)
-
-    return isValid
-  }
-  const ruleTenantId = () => validator.isInt(tenantId)
-
-  const validationResult = [
-    ruleEmail(),
-    rulePassword(),
-    ruleDisplayName(),
-    ruleTenantId(),
-  ].every(result => result === true)
-
-  return validationResult
-}
-
-const createUserToFirebase = async (email: string, password: string) => {
-  console.log(`createUserToFirebase`)
-  const { signUp } = useFirebase()
-  const user = await signUp(email, password)
-  return user
-}
-
-const onFailureCreateUserToFirebase = (error: any) => {
-  console.log(`onFailureCreateUserToFirebase`)
-  const { errMsgToStatusCodeAndMessage } = useFirebase()
-  const message = error.message
-  const { statusCode, statusMessage } = errMsgToStatusCodeAndMessage(message)
-  return { statusCode, statusMessage, message }
-}
-
-const createUserToDatabase = async (
-  uid: string,
-  email: string,
-  displayName: string,
-  tenantId: string,
-  ) => {
-  console.log(`createUserToDatabase`)
-  const prisma = new PrismaClient()
-  const profile = await prisma.profile.create({
-    data: {
-      uid,
-      email,
-      displayName,
-      tenantId: parseInt(tenantId),
-    }
-  })
-  return JSON.stringify(profile)
-}
-
-const onFailureCreateUserToDatabase = async (idToken: string) => {
-  console.log(`onFailureCreateUserToDatabase`)
-  const { deleteUser } = useFirebase()
-  await deleteUser(idToken)
-}
+/** GET /api/users */
+// router.get('/', verify, async (req, res) => {
+router.get('/', async (req, res) => {
+  const users = await prisma.profile.findMany()
+  res.send(users)
+})
 
 export default router
