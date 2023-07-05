12d27b55727790b6d38b2067de75adcb61e13a66
yukimasaki <woodenhoe@gmail.com>
2023-07-05 02:01:30 +0000

    feat: 認証処理をラップするミドルウェアを実装

--- src/backend/src/middleware/verify.ts
+++ src/backend/src/middleware/verify.ts
@@ -1,7 +1,17 @@
-import { IncomingMessage, ServerResponse } from "http"
+import { Request, Response, NextFunction } from 'express'
 import { useFirebase } from "src/utils/firebase"
 
-export const verify = async (req: IncomingMessage, seq: ServerResponse) => {
+export const verify = async (req: Request, res: Response, next: NextFunction) => {
   const { checkIdToken } = useFirebase()
-  await checkIdToken()
+  const user = await checkIdToken(req)
+
+  if (!user.error) {
+    return next()
+  }
+
+  res.send({
+    statusCode: user.error.code,
+    statusMessage: 'Unauthorized',
+    message: user.error.message,
+  })
 }
