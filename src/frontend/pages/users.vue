d242c3ff4fc22326634cfaad6a9a790405cf026b
yukimasaki <woodenhoe@gmail.com>
2023-07-06 13:02:47 +0000

    todo: テーブル実装中

--- src/frontend/pages/users.vue
+++ src/frontend/pages/users.vue
@@ -3,7 +3,8 @@
   <main class="h-full bg-base-200 p-4 md:p-6 2xl:p-10">
     <!-- Table Start-->
     <div class="flex flex-col gap-10">
-      <UsersTable />
+      <!-- <UsersTable /> -->
+      <UsersTable2 />
     </div>
     <!-- Table End-->
   </main>
@@ -12,6 +13,7 @@
 
 <script setup>
 import UsersTable from '~/components/UsersTable.vue'
+import UsersTable2 from '~/components/UsersTable2.vue'
 
 definePageMeta({
   middleware: 'required-auth'
