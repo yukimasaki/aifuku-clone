3ac209c785d5b45f723853dc28b5930c15d82cee
yukimasaki <woodenhoe@gmail.com>
2023-07-11 09:02:11 +0000

    test: page=1/5で期待したオブジェクトが返ってくることを確認

--- src/backend/src/utils/paginate.test.ts
+++ src/backend/src/utils/paginate.test.ts
@@ -43,7 +43,44 @@ describe('createPageLabels', () => {
     const pageCount = 5
 
     const pageLabels = createPageLabels(page, pageCount, pageRange, baseUrl, perPage)
-    expect(pageLabels).toEqual(['1', '2', '3', '...', '5'])
+    expect(pageLabels).toEqual([
+      {
+        "active": "true",
+        "id": "1",
+        "label": "1",
+        "url": "/api/users/?page=1&perPage=5",
+      },
+      {
+        "active": "false",
+        "id": "2",
+        "label": "2",
+        "url": "/api/users/?page=2&perPage=5",
+      },
+      {
+        "active": "false",
+        "id": "3",
+        "label": "3",
+        "url": "/api/users/?page=3&perPage=5",
+      },
+      {
+        "active": "false",
+        "id": "4",
+        "label": "...",
+        "url": "",
+      },
+      {
+        "active": "false",
+        "id": "5",
+        "label": "5",
+        "url": "/api/users/?page=5&perPage=5",
+      },
+      {
+        "active": "false",
+        "id": "6",
+        "label": "Next",
+        "url": "/api/users/?page=2&perPage=5",
+      },
+    ])
   })
 
   // it('2/2ページ', () => {
