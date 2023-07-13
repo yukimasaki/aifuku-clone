76c341a8cd8f0b16a1d8d732e198e3c5e712e243
yukimasaki <woodenhoe@gmail.com>
2023-07-13 08:30:58 +0000

    test: pass: 分岐5

--- src/backend/src/utils/paginate.test.ts
+++ src/backend/src/utils/paginate.test.ts
@@ -197,6 +197,68 @@ describe('createPageLabels', () => {
     ])
   })
 
+  it('2/6ページ', () => {
+    const pageInfo = {
+      page: 2,
+      pageCount: 6,
+      pageRange: 2,
+      perPage: 5,
+      baseUrl: '/api/users',
+    }
+
+    const pageLabels = createPageLabels(pageInfo)
+    expect(pageLabels).toEqual([
+      {
+        "active": false,
+        "id": 1,
+        "label": "Prev",
+        "url": "/api/users/?page=1&perPage=5",
+      },
+      {
+        "active": false,
+        "id": 2,
+        "label": "1",
+        "url": "/api/users/?page=1&perPage=5",
+      },
+      {
+        "active": true,
+        "id": 3,
+        "label": "2",
+        "url": "/api/users/?page=2&perPage=5",
+      },
+      {
+        "active": false,
+        "id": 4,
+        "label": "3",
+        "url": "/api/users/?page=3&perPage=5",
+      },
+      {
+        "active": false,
+        "id": 5,
+        "label": "4",
+        "url": "/api/users/?page=4&perPage=5",
+      },
+      {
+        "active": false,
+        "id": 6,
+        "label": "...",
+        "url": "",
+      },
+      {
+        "active": false,
+        "id": 7,
+        "label": "6",
+        "url": "/api/users/?page=6&perPage=5",
+      },
+      {
+        "active": false,
+        "id": 8,
+        "label": "Next",
+        "url": "/api/users/?page=3&perPage=5",
+      },
+    ])
+  })
+
   // it('5/5ページ', () => {
   //   const page = 5
   //   const pageCount = 5
@@ -204,97 +266,40 @@ describe('createPageLabels', () => {
   //   const pageLabels = createPageLabels(page, pageCount, pageRange, baseUrl, perPage)
   //   expect(pageLabels).toEqual([
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 1,
   //       "label": "Prev",
   //       "url": "/api/users/?page=4&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 2,
   //       "label": "1",
   //       "url": "/api/users/?page=1&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 3,
   //       "label": "...",
   //       "url": "",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 4,
   //       "label": "3",
   //       "url": "/api/users/?page=3&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 5,
   //       "label": "4",
   //       "url": "/api/users/?page=4&perPage=5",
   //     },
   //     {
-  //       "active": "true",
+  //       "active": true,
   //       "id": 6,
   //       "label": "5",
   //       "url": "/api/users/?page=5&perPage=5",
-  //     },
-  //   ])
-  // })
-
-  // it('2/6ページ', () => {
-  //   const page = 2
-  //   const pageCount = 6
-
-  //   const pageLabels = createPageLabels(page, pageCount, pageRange, baseUrl, perPage)
-  //   expect(pageLabels).toEqual([
-  //     {
-  //       "active": "false",
-  //       "id": 1,
-  //       "label": "Prev",
-  //       "url": "/api/users/?page=1&perPage=5",
-  //     },
-  //     {
-  //       "active": "false",
-  //       "id": 2,
-  //       "label": "1",
-  //       "url": "/api/users/?page=1&perPage=5",
-  //     },
-  //     {
-  //       "active": "true",
-  //       "id": 3,
-  //       "label": "2",
-  //       "url": "/api/users/?page=2&perPage=5",
-  //     },
-  //     {
-  //       "active": "false",
-  //       "id": 4,
-  //       "label": "3",
-  //       "url": "/api/users/?page=3&perPage=5",
-  //     },
-  //     {
-  //       "active": "false",
-  //       "id": 5,
-  //       "label": "4",
-  //       "url": "/api/users/?page=4&perPage=5",
-  //     },
-  //     {
-  //       "active": "false",
-  //       "id": 6,
-  //       "label": "...",
-  //       "url": "",
-  //     },
-  //     {
-  //       "active": "false",
-  //       "id": 7,
-  //       "label": "6",
-  //       "url": "/api/users/?page=6&perPage=5",
-  //     },
-  //     {
-  //       "active": "false",
-  //       "id": 8,
-  //       "label": "Next",
-  //       "url": "/api/users/?page=3&perPage=5",
   //     },
   //   ])
   // })
@@ -306,49 +311,49 @@ describe('createPageLabels', () => {
   //   const pageLabels = createPageLabels(page, pageCount, pageRange, baseUrl, perPage)
   //   expect(pageLabels).toEqual([
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 1,
   //       "label": "Prev",
   //       "url": "/api/users/?page=2&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 2,
   //       "label": "1",
   //       "url": "/api/users/?page=1&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 3,
   //       "label": "2",
   //       "url": "/api/users/?page=2&perPage=5",
   //     },
   //     {
-  //       "active": "true",
+  //       "active": true,
   //       "id": 4,
   //       "label": "3",
   //       "url": "/api/users/?page=3&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 5,
   //       "label": "4",
   //       "url": "/api/users/?page=4&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 6,
   //       "label": "5",
   //       "url": "/api/users/?page=5&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 7,
   //       "label": "6",
   //       "url": "/api/users/?page=6&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 8,
   //       "label": "Next",
   //       "url": "/api/users/?page=4&perPage=5",
@@ -363,67 +368,67 @@ describe('createPageLabels', () => {
   //   const pageLabels = createPageLabels(page, pageCount, pageRange, baseUrl, perPage)
   //   expect(pageLabels).toEqual([
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 1,
   //       "label": "Prev",
   //       "url": "/api/users/?page=4&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 2,
   //       "label": "1",
   //       "url": "/api/users/?page=1&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 3,
   //       "label": "...",
   //       "url": "",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 4,
   //       "label": "3",
   //       "url": "/api/users/?page=3&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 5,
   //       "label": "4",
   //       "url": "/api/users/?page=4&perPage=5",
   //     },
   //     {
-  //       "active": "true",
+  //       "active": true,
   //       "id": 6,
   //       "label": "5",
   //       "url": "/api/users/?page=5&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 7,
   //       "label": "6",
   //       "url": "/api/users/?page=6&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 8,
   //       "label": "7",
   //       "url": "/api/users/?page=7&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 9,
   //       "label": "...",
   //       "url": "",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 10,
   //       "label": "9",
   //       "url": "/api/users/?page=9&perPage=5",
   //     },
   //     {
-  //       "active": "false",
+  //       "active": false,
   //       "id": 11,
   //       "label": "Next",
   //       "url": "/api/users/?page=6&perPage=5",
