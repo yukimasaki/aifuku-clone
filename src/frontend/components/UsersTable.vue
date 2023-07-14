cb4e72cab1381e24d4aedff63e563f04ae4b4ddd
yukimasaki <woodenhoe@gmail.com>
2023-07-14 08:53:43 +0000

    feat: ページネータを水平中央に固定

--- src/frontend/components/UsersTable.vue
+++ src/frontend/components/UsersTable.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="container max-w-full overflow-x-auto whitespace-nowrap">
-    <table class="table table-lg bg-white rounded-sm text-lg">
-      <thead>
-        <tr>
-          <th v-for="testHeader in testHeaders" :key="testHeader.id">
-            {{ testHeader.label }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users.items" :key="user.id">
-          <td class="w-12">
-            <label>
-              <input type="checkbox" class="checkbox">
-            </label>
-          </td>
-          <td class="w-12">{{ user.id }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.displayName }}</td>
-          <td class="w-12">{{ user.tenantId }}</td>
-          <td class="w-12"><nuxt-link to="#">Edit</nuxt-link></td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <div class="container max-w-full overflow-x-auto whitespace-nowrap">
+      <table class="table table-lg bg-white rounded-sm text-lg">
+        <thead>
+          <tr>
+            <th v-for="testHeader in testHeaders" :key="testHeader.id">
+              {{ testHeader.label }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users.items" :key="user.id">
+            <td class="w-12">
+              <label>
+                <input type="checkbox" class="checkbox">
+              </label>
+            </td>
+            <td class="w-12">{{ user.id }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.displayName }}</td>
+            <td class="w-12">{{ user.tenantId }}</td>
+            <td class="w-12"><nuxt-link to="#">Edit</nuxt-link></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
-    <div class="join">
-      <nuxt-link
-        v-for="link in users.links" :key="link.id"
-        :to="link.url"
-      >
-        <button class="join-item btn">
-          {{ link.label }}
-        </button>
-      </nuxt-link>
+    <div class="flex justify-center">
+      <div class="join">
+        <nuxt-link
+          v-for="link in users.links" :key="link.id"
+          :to="link.url"
+        >
+          <button class="join-item btn">
+            {{ link.label }}
+          </button>
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
