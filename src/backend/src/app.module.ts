135168f20710f50d9b238aa4cf7a03fc6743bc05
yukimasaki <woodenhoe@gmail.com>
2023-07-17 23:42:45 +0000

    feat: profiles作成

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [],
+  imports: [ProfilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
