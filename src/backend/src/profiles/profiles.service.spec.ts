12404d3ea0115324f57ef514b7a038f1bbf25b71
yukimasaki <woodenhoe@gmail.com>
2023-07-18 06:30:28 +0000

    test: delete: 全ユーザ取得

--- src/backend/src/profiles/profiles.service.spec.ts
+++ src/backend/src/profiles/profiles.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesService } from './profiles.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { Profile } from '@prisma/client';
 
 describe('ProfilesService', () => {
   let service: ProfilesService;
@@ -16,16 +15,6 @@ describe('ProfilesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-
-  it('全ユーザ取得', async () => {
-    const profiles = await service.findAll();
-    expect(Array.isArray(profiles)).toBe(true);
-    expect(profiles.every((profile: Profile) => typeof profile.id === 'number')).toBe(true);
-    expect(profiles.every((profile: Profile) => typeof profile.uid === 'string')).toBe(true);
-    expect(profiles.every((profile: Profile) => typeof profile.email === 'string')).toBe(true);
-    expect(profiles.every((profile: Profile) => typeof profile.displayName === 'string')).toBe(true);
-    expect(profiles.every((profile: Profile) => typeof profile.tenantId === 'number')).toBe(true);
   });
 
   it('ユーザを1レコード取得', async () => {
