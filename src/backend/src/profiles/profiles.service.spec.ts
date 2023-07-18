a4b9dca6cae55be482e9b6ebb8d2205b3433e2bf
yukimasaki <woodenhoe@gmail.com>
2023-07-18 01:09:43 +0000

    test: findAllのテストを実装中

--- src/backend/src/profiles/profiles.service.spec.ts
+++ src/backend/src/profiles/profiles.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesService } from './profiles.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { Profile } from '@prisma/client';
+import { profile } from 'console';
 
 describe('ProfilesService', () => {
   let service: ProfilesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfilesService],
+      providers: [PrismaService, ProfilesService],
     }).compile();
 
     service = module.get<ProfilesService>(ProfilesService);
@@ -14,5 +17,11 @@ describe('ProfilesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('全ユーザ取得', async () => {
+    const profiles = await service.findAll();
+    expect(Array.isArray(profiles)).toBe(true);
+    expect(profiles.every((profile: Profile) => typeof profile.id === 'number')).toBe(true)
   });
 });
