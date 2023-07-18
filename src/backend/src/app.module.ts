312adcda325ff8d2fda4e4bf170be688a79c9308
yukimasaki <woodenhoe@gmail.com>
2023-07-18 02:50:26 +0000

    fix: ビルド時のエラー解消

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ProfilesModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
