4feb7d18774577d68e2c838f31379d6e30ecf316
yukimasaki <woodenhoe@gmail.com>
2023-07-19 09:02:24 +0000

    onfix: グローバルなHTTP例外フィルタを実装中

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
