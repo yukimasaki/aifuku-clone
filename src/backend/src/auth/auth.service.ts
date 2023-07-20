e3c0f9fe00301fc0477539ea172757aea8d739b9
yukimasaki <woodenhoe@gmail.com>
2023-07-20 09:04:59 +0000

    feat: ユーザ検証とJWT返却処理を分割

--- src/backend/src/auth/auth.service.ts
+++ src/backend/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'src/common/prisma/prisma.service';
 import { UserCredentialsDto } from './auth.entity';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { UserJwtPayload } from 'src/users/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -11,10 +12,11 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async signIn(
-    userCredentialsDto: UserCredentialsDto
-  ): Promise<{ accessToken: string }> {
-    const { email, password } = userCredentialsDto;
+  async validateUser(
+    email: UserCredentialsDto['email'],
+    password: UserCredentialsDto['password'],
+  ): Promise<UserJwtPayload | null> {
+    console.log(`bbb`)
     const user = await this.prisma.user.findUnique({
       where: { email }
     });
@@ -23,12 +25,19 @@ export class AuthService {
       user &&
       await bcrypt.compare(password, user.hashedPassword)
     ) {
-      const payload = { id: user.id, email: user.email }
-      const accessToken = this.jwtService.sign(payload);
-      return { accessToken }
+      const { hashedPassword, ...userWithoutHashedPassword } = user;
+      return userWithoutHashedPassword;
     }
-    throw new UnauthorizedException(
-      'ユーザ名またはパスワードを確認してください',
-    )
+
+    return null
+  }
+
+  async createJwt(
+    user: UserJwtPayload
+  ): Promise<{ accessToken: string }> {
+
+    const payload = { id: user.id, email: user.email }
+    const accessToken = this.jwtService.sign(payload);
+    return { accessToken }
   }
 }
