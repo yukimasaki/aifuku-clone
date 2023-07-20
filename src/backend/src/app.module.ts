962db2725ea9d43054faf42e3d0ba8029bd2baa8
yukimasaki <woodenhoe@gmail.com>
2023-07-20 05:49:46 +0000

    feat: auth.controller, serviceを作成

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -3,12 +3,15 @@ import { UsersModule } from './users/users.module';
 import { PrismaService } from './common/prisma/prisma.service';
 import { PaginatorService } from './common/paginator/paginator.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
+    AuthModule,
   ],
-  providers: [PrismaService, PaginatorService],
+  providers: [PrismaService, PaginatorService, AuthService],
 })
 export class AppModule {}
