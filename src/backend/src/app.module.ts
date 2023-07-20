572b24fe060145097dfb731320d26ec915e3ad6b
yukimasaki <woodenhoe@gmail.com>
2023-07-20 05:41:26 +0000

    feat: JWT生成処理を追加

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './common/prisma/prisma.service';
 import { PaginatorService } from './common/paginator/paginator.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+  ],
   providers: [PrismaService, PaginatorService],
 })
 export class AppModule {}
--- src/backend/src/users/users.module.ts
+++ src/backend/src/users/users.module.ts
@@ -3,8 +3,25 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../common/prisma/prisma.service';
 import { PaginatorService } from '../common/paginator/paginator.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET_KEY'),
+          signOptions: {
+            expiresIn: 3600,
+          },
+        }
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService, PrismaService, PaginatorService]
 })
