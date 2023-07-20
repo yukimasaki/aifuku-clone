c8252b86c36c7b4f60e127b580339f785b9b621a
yukimasaki <woodenhoe@gmail.com>
2023-07-20 09:10:12 +0000

    faet: providersにLocalStrategyを追加

--- src/backend/src/auth/auth.module.ts
+++ src/backend/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
@@ -22,6 +23,6 @@ import { PrismaService } from 'src/common/prisma/prisma.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, LocalStrategy],
 })
 export class AuthModule {}
