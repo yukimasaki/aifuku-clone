5cf3d371bfa0cc4c5a14345f1593ddb772483756
yukimasaki <woodenhoe@gmail.com>
2023-07-24 05:51:31 +0000

    feat: エラーメッセージを用意

---
+++ src/backend/src/common/errors/error.constants.ts
@@ -0,0 +1,10 @@
+export const VALIDATION_ERRORS = {
+  INVALID_EMAIL: 'メールアドレスの形式を満たしていません',
+  INVALID_PASSWORD_LOWER_LETTER: 'パスワードには小文字を含めてください',
+  INVALID_PASSWORD_UPPER_LETTER: 'パスワードには大文字を含めてください',
+  INVALID_PASSWORD_NUMBER: 'パスワードには数字を含めてください',
+  INVALID_PASSWORD_SPECIAL_CHARACTOR: 'パスワードには記号を含めてください',
+  INVALID_PASSWORD_LENGTH: 'パスワードの文字数が足りません',
+  REQUIRED: '必ず入力してください',
+  NOT_MATCH_PASSWORD: '入力されたパスワードが一致しません',
+}
