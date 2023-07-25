0d9bb896bc545cf6422cab63d2fc349b2dbde088
yukimasaki <woodenhoe@gmail.com>
2023-07-25 08:43:26 +0000

    chore: daisyuiオプション

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,9 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+const customColor = {
+  primary: '#5f939a',
+  secondary: "#1b2021",
+  accent: "#d8ac9c",
+  neutral: "#eac8af",
+  base: "#eff4f4",
+  info: "#4e8dda",
+  success: "#1a7465",
+  warning: "#9f730f",
+  error: "#f33f42",
 }
 
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        "daisy-primary": customColor.primary,
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        aifuku: {
+          "primary": customColor.primary,
+          "secondary": customColor.secondary,
+          "accent": customColor.accent,
+          "neutral": customColor.neutral,
+          "base-100": customColor.base,
+          "info": customColor.info,
+          "success": customColor.success,
+          "warning": customColor.warning,
+          "error": customColor.error,
+        }
+      }
+    ]
+  },
+}
