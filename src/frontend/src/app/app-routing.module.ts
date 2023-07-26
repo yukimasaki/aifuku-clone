9e96a70ad4d9e5adf5dc2a540d23762b698d26ae
yukimasaki <woodenhoe@gmail.com>
2023-07-26 06:21:43 +0000

    feat: 存在しないルートへのルーティング、パス無指定時のルーティングを追加

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
