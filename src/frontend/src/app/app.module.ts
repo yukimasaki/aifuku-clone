f24ec0829e68d7193d6d814e8e0acf2d29e46a97
yukimasaki <woodenhoe@gmail.com>
2023-07-26 08:12:26 +0000

    issue: 初期化処理に問題があるのかコンポーネントが描画されない

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -11,6 +10,8 @@ import { StoreModule } from '@ngrx/store';
 import { sidebarReducer } from 'src/store/sidebar/sidebar.reducer';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from './login-button/login-button.component';
 
 @NgModule({
   declarations: [
@@ -18,10 +19,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     SidebarComponent,
     HeaderComponent,
     NotFoundComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginButtonComponent,
   ],
   imports: [
     BrowserModule,
+    AuthModule.forRoot({
+      domain: 'dev-aifuku.jp.auth0.com',
+      clientId: 'hUzhT4Bxfb1MHBjJSeuhvRWvAKIjw9hm',
+      authorizationParams: {
+        redirect_uri: `${window.location.origin}`
+      }
+    }),
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
