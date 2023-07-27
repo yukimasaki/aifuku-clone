5e50c3d79c9cc6a664a6b7a1586ab07eb05ba4b7
yukimasaki <woodenhoe@gmail.com>
2023-07-27 02:49:47 +0000

    dashboardとwelcomeへのルーティング、AuthGuard追加

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from '@auth0/auth0-angular';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: '**', component: NotFoundComponent },
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
