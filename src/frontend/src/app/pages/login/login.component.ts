df2a3e1626c8695405ceb57dc947055f67e076c4
yukimasaki <woodenhoe@gmail.com>
2023-07-28 07:50:11 +0000

    feat: サインアウト時のレイアウトを使用するページにレイアウトを適用

--- src/frontend/src/app/pages/login/login.component.ts
+++ src/frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HeaderSignedOutService } from 'src/app/components/header-signed-out/header-signed-out.service';
+import { HeaderService } from 'src/app/components/header/header.service';
 import { SidebarService } from 'src/app/components/sidebar/sidebar.service';
 
 @Component({
@@ -8,14 +10,20 @@ import { SidebarService } from 'src/app/components/sidebar/sidebar.service';
 })
 export class LoginComponent {
   constructor(
-    private sidebarService: SidebarService
+    private sidebarService: SidebarService,
+    private headerService: HeaderService,
+    private headerSignedOutService: HeaderSignedOutService,
   ) {}
 
   ngOnInit(): void {
     this.sidebarService.hide();
+    this.headerService.hide();
+    this.headerSignedOutService.show();
   }
 
   ngOnDestroy(): void {
     this.sidebarService.show();
+    this.headerService.show();
+    this.headerSignedOutService.hide();
   }
 }
--- src/frontend/src/app/pages/not-found/not-found.component.ts
+++ src/frontend/src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HeaderSignedOutService } from 'src/app/components/header-signed-out/header-signed-out.service';
+import { HeaderService } from 'src/app/components/header/header.service';
+import { SidebarService } from 'src/app/components/sidebar/sidebar.service';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class NotFoundComponent {
+  constructor(
+    private sidebarService: SidebarService,
+    private headerService: HeaderService,
+    private headerSignedOutService: HeaderSignedOutService,
+  ) {}
 
+  ngOnInit(): void {
+    this.sidebarService.hide();
+    this.headerService.hide();
+    this.headerSignedOutService.show();
+  }
+
+  ngOnDestroy(): void {
+    this.sidebarService.show();
+    this.headerService.show();
+    this.headerSignedOutService.hide();
+  }
 }
