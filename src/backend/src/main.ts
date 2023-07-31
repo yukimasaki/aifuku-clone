b8109b97740003d9892eb0088c915d922b31b63f
yukimasaki <woodenhoe@gmail.com>
2023-07-31 08:22:38 +0000

    feat: httpsOptionsを追記、開発用の証明書を配置

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('ssl/server.cert'),
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
