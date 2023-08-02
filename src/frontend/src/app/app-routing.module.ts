7a879a635ea130c9b799f9b2d39f240b85801d5c
yukimasaki <woodenhoe@gmail.com>
2023-08-02 01:07:00 +0000

    feat: app配下のimportを絶対パスに変更

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { LoginComponent } from './pages/login/login.component';
+
+import { NotFoundComponent } from '~/pages/not-found/not-found.component';
+import { DashboardComponent } from '~/pages/dashboard/dashboard.component';
+import { WelcomeComponent } from '~/pages/welcome/welcome.component';
+import { LoginComponent } from '~/pages/login/login.component';
 
 const routes: Routes = [
   {
--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { StoreModule } from '@ngrx/store';
-import { sidebarReducer } from '~/store/sidebar/sidebar.reducer';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { LoginComponent } from './pages/login/login.component';
-import { HeaderSignedOutComponent } from './components/header-signed-out/header-signed-out.component';
-import { LoginFormComponent } from './components/forms/login-form/login-form.component';
-import { ClassValidatorFormBuilderModule } from 'ngx-reactive-form-class-validator';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { ClassValidatorFormBuilderModule } from 'ngx-reactive-form-class-validator';
 
+import { AppRoutingModule } from '~/app-routing.module';
+import { AppComponent } from '~/app.component';
+import { SidebarComponent } from '~/components/sidebar/sidebar.component';
+import { HeaderComponent } from '~/components/header/header.component';
+import { HeaderSignedOutComponent } from '~/components/header-signed-out/header-signed-out.component';
+import { LoginFormComponent } from '~/components/forms/login-form/login-form.component';
+import { NotFoundComponent } from '~/pages/not-found/not-found.component';
+import { DashboardComponent } from '~/pages/dashboard/dashboard.component';
+import { WelcomeComponent } from '~/pages/welcome/welcome.component';
+import { LoginComponent } from '~/pages/login/login.component';
+import { sidebarReducer } from '~/store/sidebar/sidebar.reducer';
 
 @NgModule({
   declarations: [
